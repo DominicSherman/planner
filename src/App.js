0f38ecda01be5c58abcb0fc75ba76534c51b8c01
DominicSherman <dominic.sherman98@gmail.com>
2018-12-14 10:23:21 -0600

    lets get it started hah

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
 import './css/App.css';
+import moment from 'moment';
+import {green, lightGray, white} from './constants/style-variables';
 
 export default class App extends Component {
     render() {
+        let days = [];
+
+        for (let i = 1; i <= 7; i++) {
+            days = [...days, moment().isoWeekday(i)];
+        }
+
+        days.forEach((day) => console.log(day.format('dddd, MMMM Do')));
+
         return (
-            <div className="App">
-                <p>{'here we go'}</p>
+            <div className={'App'}>
+                {
+                    days.map((day) =>
+                        <div
+                            className={'App-day'}
+                            style={{backgroundColor: day.get('day') === moment().get('day') ? green : white}}
+                        >
+                            <p>{day.format('dddd, MMMM Do')}</p>
+                        </div>
+                    )
+                }
             </div>
         );
     }
